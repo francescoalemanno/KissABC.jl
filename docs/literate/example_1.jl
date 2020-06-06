77944c9712b6e0d8fe9fd3468858aadfeabafcea
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-06-06 18:50:47 +0200

    wip example

--- docs/literate/example_1.jl
+++ docs/literate/example_1.jl
@@ -1,6 +1,7 @@
 # # A gaussian mixture model
 # First of all we define our model,
 using KissABC
+using Distributions
 
 function model(P,N)
     μ_1, μ_2, σ_1, σ_2, prob=P
@@ -13,7 +14,9 @@ function model(P,N)
     R
 end
 
-data=model((1.0,0.0,0.2,2.0,0.4),10000)
+# Let's use the model to generate some data, this data will constitute our dataset
+parameters = (1.0, 0.0, 0.2, 2.0, 0.4)
+data=model(parameters,1000)
 
 # let's look at the data
 
@@ -23,4 +26,41 @@ savefig("ex1_hist1.svg"); nothing # hide
 
 # ![ex1_hist1](ex1_hist1.svg)
 
-we can now try to infer all parameters using `KissABC`
+# we can now try to infer all parameters using `KissABC`, first of all we need to define a reasonable prior for our model
+
+prior=Factored(
+            Uniform(0,3),
+            Uniform(-2,2),
+            Uniform(0,1),
+            Uniform(0,4),
+            Beta(4,4)
+        )
+
+# a sample from the prior
+rand(prior)
+# now we need a distance function to compare datasets
+function D(x,y)
+    r=0:0.01:1
+    sum(abs2,quantile.(Ref(x),r).-quantile.(Ref(y),r))/length(r)
+end
+
+# we can now run ABCDE to get the posterior distribution of our parameters given the dateset `data`
+res,Δ=ABCDE(prior,model,data,D,0.02,params=1000,parallel=true,verbose=false)
+
+# let's see the median and 95% confidence interval for the inferred parameters and let's compare them with the true values
+function getstats(P,V)
+    (
+        param=P,
+        median=median(V),
+        lowerbound=quantile(V,0.05),
+        upperbound=quantile(V,0.95)
+    )
+end
+
+stats=getstats.((:μ_1, :μ_2, :σ_1, :σ_2, :prob),[getindex.(res,i) for i in 1:5])
+
+for is in eachindex(stats)
+    println(parameters[is], " → ", stats[is])
+end
+
+# we can see that the true values lie inside the confidence interval.
