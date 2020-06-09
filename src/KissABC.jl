d2b0a9fbf0e070e8fe0dceafdbc3fa8b7be8b06c
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-06-09 22:09:22 +0200

    add header doc to KissABC

--- src/KissABC.jl
+++ src/KissABC.jl
@@ -1,3 +1,25 @@
+"""
+    KissABC
+Module to perform approximate bayesian computation,
+
+Simple Example:
+inferring the mean of a `Normal` distribution
+```julia
+using KissABC
+using Distributions
+
+prior=Normal(0,1)
+data=randn(1000) .+ 1
+sim(μ,other)=randn(1000) .+ μ
+dist(x,y) = abs(mean(x) - mean(y))
+
+plan=ABCplan(prior, sim, data, dist)
+μ_post,Δ = ABCDE(plan, 1e-2)
+@show mean(μ_post) ≈ 1.0
+```
+
+for more complicated code examples look at `https://github.com/francescoalemanno/KissABC.jl/`
+"""
 module KissABC
 
 using Base.Threads
